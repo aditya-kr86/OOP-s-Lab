7cd7be4f5cb44dfebecc787aecfab51baae060dc
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-02-25 23:05:58 +0530

    Create q10.cpp

---
+++ Assignment 25-02-2025/q10.cpp
@@ -0,0 +1,12 @@
+// Write a C++ Program to Find ASCII Value of a Character
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    cout<<"Enter a Character\n";
+    cin>>c;
+    cout<<"The ASCII value of "<< c <<" is "<<int(c);
+    return 0;
+}
