29745b591f26f1b96b8c59d71c6bf888a02bfa30
Aditya Kumar <adityakumargupta082003@gmail.com>
2025-02-25 23:08:02 +0530

    Update q2.cpp

--- Assignment 25-02-2025/q2.cpp
+++ Assignment 25-02-2025/q2.cpp
@@ -19,95 +19,7 @@ int main()
 }
 
 
-
-
-
-
-// Write a C++ Program to Find GCD.
-#include <iostream>
-#include<math.h>
-using namespace std;
-
-int main()
-{
-    int first_number, second_number;
-
-    cout<<"Enter Two Number of Which You Want To find GCD\n";
-    cin>>first_number>>second_number;
-    int greater_number = max(first_number, second_number);
-    int n = greater_number/2;
-    int gcd;
-    for (int i = 2; i <= n; i++)
-    {
-        if (first_number % i == 0 && second_number % i == 0)
-        {
-            gcd = i;
-        }   
-    }
-    cout<<"The GCD of "<<first_number<<" and "<<second_number<<" is "<<gcd<<"\n";
-
-    
-    return 0;
-}
-
-
-
-
-
-// Write a C++ Program to Reverse a Number
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int remainder, number,reversed_number = 0;
-    cout<<"Enter a Number to Which You Want to Reverse\n";
-    cin>>number;
-    while (number > 0)
-    {
-        remainder =  number%10 ;
-        reversed_number = reversed_number * 10 + remainder;
-        number = number/10;
-    }
-    cout<<"The Reversed Number is "<<reversed_number;
-    return 0;
-}
-
-
-
-
-
-
-
-
-// Write a C++ Program to calculate Factorial of a Number Using Recursion
-#include <iostream>
-using namespace std;
-int factorial(int number)
-{
-    if (number == 0)
-    {
-        return 1;
-    }
-    else
-        return number * factorial(number - 1); 
-}
-int main()
-{
-    int number;
-    cout<<"Enter a Number of Which You Want To Find Factorial: ";
-    cin>>number;
-    cout<<"The Factorial of Number "<<number<<" is "<< factorial(number);
-    return 0;
-}
-
-
-
-
-
-
-
-// Write a C++ Program to Add Complex Numbers by Passing Structure to a function
+// Q.6 Write a C++ Program to Add Complex Numbers by Passing Structure to a function
 
 
 
@@ -119,7 +31,7 @@ int main()
 
 
 
-// Write a C++ Program to check Armstrong Number
+// Q.7 Write a C++ Program to check Armstrong Number
 
 
 
@@ -129,7 +41,7 @@ int main()
 
 
 
-// Write a C++ Program to Find Fibonacci Number using Iterative and Recursive
+// Q.8 Write a C++ Program to Find Fibonacci Number using Iterative and Recursive
 
 
 
@@ -137,7 +49,7 @@ int main()
 
 
 
-// Write a C++ Program to Check Whether a Character is Vowel or Consonant
+// Q.9 Write a C++ Program to Check Whether a Character is Vowel or Consonant
 #include <iostream>
 using namespace std;
 
@@ -163,27 +75,3 @@ int main()
     }
     return 0;
 }
-
-
-
-
-
-// Write a C++ Program to Find ASCII Value of a Character
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    char c;
-    cout<<"Enter a Character\n";
-    cin>>c;
-    cout<<"The ASCII value of "<< c <<" is "<<int(c);
-    return 0;
-}
-
-
-
-
-
-
-
